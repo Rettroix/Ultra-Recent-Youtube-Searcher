b95f3617542443dfe2702fc30a607758029a1dad
Rettroix <drhoix@yahoo.co.uk>
2016-09-19 22:02:43 +0100

    change time to today

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ $(function() {
             q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
             maxResults: 3,
             order: "date",
-            publishedAfter: "2015-01-01T00:00:00Z"
        }); 
        // execute the request
        request.execute(function(response) {
